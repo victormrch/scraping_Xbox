ee0fc3323c7c5db790f6332e3f1acacbf82438f7
Victor Ramon <victormanuelchica@gmail.com>
2022-04-12 19:46:30 +0200

    Add express to app

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-require("dotenv").config();
-var cron = require("node-cron");
+const express = require("express");
 
-const { chromium } = require("playwright");
-const axios = require("axios");
+const app = express();
 
-const shops = [
-  {
-    vendor: "Game",
-    url: "https://www.game.es/OFERTAS/PACK/PACKS/PACK-SEMINUEVO-NINTENDO-SWITCH-2-JOY-CON-A-ELEGIR/P02075",
-    checkStock: async ({ page }) => {
-      const content = await page.textContent(".product-quick-actions");
-      return content.includes("Producto no disponible") === false;
+let PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+  require("dotenv").config();
+  var cron = require("node-cron");
+
+  const { chromium } = require("playwright");
+  const axios = require("axios");
+
+  const shops = [
+    {
+      vendor: "Game",
+      url: "https://www.game.es/OFERTAS/PACK/PACKS/PACK-SEMINUEVO-NINTENDO-SWITCH-2-JOY-CON-A-ELEGIR/P02075",
+      checkStock: async ({ page }) => {
+        const content = await page.textContent(".product-quick-actions");
+        return content.includes("Producto no disponible") === false;
+      },
     },
-  },
-];
+  ];
 
-cron.schedule("*/1 * * * *", () => {
-  (async () => {
-    const browser = await chromium.launch();
+  cron.schedule("*/1 * * * *", () => {
+    (async () => {
+      const browser = await chromium.launch();
 
-    for (const shop of shops) {
-      const { checkStock, vendor, url } = shop;
-      const page = await browser.newPage();
-      await page.goto(url);
+      for (const shop of shops) {
+        const { checkStock, vendor, url } = shop;
+        const page = await browser.newPage();
+        await page.goto(url);
 
-      const hasStock = await checkStock({ page, url });
+        const hasStock = await checkStock({ page, url });
 
-      console.log(
-        `${vendor}: ${hasStock ? "In Stock 💸 " : "Out of stock 😢 "}`
-      );
+        console.log(
+          `${vendor}: ${hasStock ? "In Stock 💸 " : "Out of stock 😢 "}`
+        );
 
-      if (hasStock) {
-        await axios.post(process.env.URL_DISCORD, {
-          content:
-            "🏃🏻‍♂️🏃🏻‍♂️Run!!!. There are stock in Game!!!! Buy the Xbox Serie - X, NOW!!!! Buy Link: 'https://bit.ly/3vd5Xyb'  ",
-        });
+        if (hasStock) {
+          await axios.post(process.env.URL_DISCORD, {
+            content:
+              "🏃🏻‍♂️🏃🏻‍♂️Run!!!. There are stock in Game!!!! Buy the Xbox Serie - X, NOW!!!! Buy Link: 'https://bit.ly/3vd5Xyb'  ",
+          });
+        }
       }
-    }
 
-    await browser.close();
-  })();
+      await browser.close();
+    })();
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
